45a3bfe2e2d89f7a040aaa517ad432e85d81b025
Ruslan <akhundov2@gmail.com>
2013-09-07 17:02:32 +0400

    uodate

--- lifeProject/life/src/main/java/com/example/life/WhirlActivity.java
+++ lifeProject/life/src/main/java/com/example/life/WhirlActivity.java
@@ -3,113 +3,126 @@ package com.example.life;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.view.View;
+import android.view.SurfaceHolder;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.graphics.Color;
 import java.util.Random;
 
 
 public class WhirlActivity extends Activity {
 
 
-    class WhirlView extends View {
+    class WhirlView extends SurfaceView implements Runnable {
         Paint p;
         int WIDTH = 240;
         int HEIGHT = 320;
+        int num_of_colors = 16;
         int[][] field;
-        int[][] new_step_field;
         int[][] colors;
-        Recounter rec;
-        Thread thread;
+        Thread thread = null;
+        volatile boolean thread_is_running = true;
+        int cnt = 0;
+        SurfaceHolder surfaceHolder;
+        long start_time, end_time;
+
 
         public WhirlView(Context context) {
             super(context);
             p = new Paint();
             field = new int[WIDTH][HEIGHT];
-            new_step_field = new int[WIDTH][HEIGHT];
-            colors = new int[16][4];
             Random rand = new Random();
-            for (int i = 0; i < 16; i++) {
-                for (int j = 0; j < 4; j++) {
+            colors = new int[num_of_colors][4];
+            for(int i = 0; i < num_of_colors; ++i){
+                for(int j = 0; j < 4; ++j){
                     colors[i][j] = rand.nextInt(256);
                 }
             }
-            for (int i = 0; i < WIDTH; i++)
-                for (int j = 0; j < HEIGHT; j++) {
-                    field[i][j] = rand.nextInt(16);
+            for (int i = 0; i < WIDTH; ++i)
+                for (int j = 0; j < HEIGHT; ++j) {
+                    field[i][j] = rand.nextInt(num_of_colors);
                 }
-            rec = new Recounter();
-            thread = new Thread(rec);
+            surfaceHolder = getHolder();
+            thread = new Thread(this);
+            start_time = System.currentTimeMillis();
             thread.start();
         }
 
         @Override
         public void onDraw(Canvas canvas) {
-
             for (int i = 0; i < WIDTH; ++i) {
                 for (int j = 0; j < HEIGHT; ++j) {
-                    field[i][j] = new_step_field[i][j];
-                }
-            }
-            rec.notifyAll();
-            for (int i = 0; i < WIDTH; i++) {
-                for (int j = 0; j < HEIGHT; j++) {
-                    p.setARGB(colors[field[i][j]][0], colors[field[i][j]][1],
-                            colors[field[i][j]][2], colors[field[i][j]][3]);
-                    canvas.drawPoint(i, j, p);
+                    p.setARGB(colors[field[i][j]][0], colors[field[i][j]][1], colors[field[i][j]][2], colors[field[i][j]][3]);
                     canvas.drawRect(3 * i, 4 * j, 3 * (i + 1), 4 * (j + 1), p);
+
+                }
+            }
+            p.setColor(Color.BLACK);
+            canvas.drawRect(0, 0, 100, 50, p);
+            p.setColor(Color.YELLOW);
+            p.setTextSize(20);
+            end_time = System.currentTimeMillis();
+            canvas.drawText("FPS: " + 1000 / (end_time - start_time), 10, 30, p);
+            start_time = end_time;
+        }
+
+
+
+        public void recount() {
+
+            int[][] new_step_feild = new int[WIDTH][HEIGHT];
+            for (int i = 0; i < WIDTH; ++i) {
+                for (int j = 0; j < HEIGHT; ++j) {
+                    new_step_feild[i][j] = field[i][j];
+                }
+            }
+            for (int i = 0; i < WIDTH; ++i) {
+                for (int j = 0; j < HEIGHT; ++j) {
+                    if (i < WIDTH - 1 && (field[i + 1][j] + 1) % 16 == field[i][j]) {
+                        new_step_feild[i][j] = field[i + 1][j];
+                    } else if (j < HEIGHT - 1 && (field[i][j + 1] + 1) % 16 == field[i][j]) {
+                        new_step_feild[i][j] = field[i][j + 1];
+                    } else if (i < WIDTH - 1 && j < HEIGHT - 1 && (field[i + 1][j + 1] + 1) % 16 == field[i][j]) {
+                        new_step_feild[i][j] = field[i + 1][j + 1];
+                    } else if (i > 0 && (field[i - 1][j] + 1) % 16 == field[i][j]) {
+                        new_step_feild[i][j] = field[i - 1][j];
+                    } else if (j > 0 && (field[i][j - 1] + 1) % 16 == field[i][j]) {
+                        new_step_feild[i][j] = field[i][j - 1];
+                    } else if (i > 0 && j > 0 && (field[i - 1][j - 1] + 1) % 16 == field[i][j]) {
+                        new_step_feild[i][j] = field[i - 1][j - 1];
+                    } else if (i > 0 && j < HEIGHT - 1 && (field[i - 1][j + 1] + 1) % 16 == field[i][j]) {
+                        new_step_feild[i][j] = field[i - 1][j + 1];
+                    } else if (i < WIDTH - 1 && j > 0 && (field[i + 1][j - 1] + 1) % 16 == field[i][j]) {
+                        new_step_feild[i][j] = field[i + 1][j - 1];
+                    }
+
+                }
+
+                field = new_step_feild;
+            }
+        }
+
+        public void run() {
+            while (thread_is_running) {
+                if (surfaceHolder.getSurface().isValid()) {
+                    Canvas canvas = surfaceHolder.lockCanvas();
+                    recount();
+                    onDraw(canvas);
+                    surfaceHolder.unlockCanvasAndPost(canvas);
                 }
             }
 
-            invalidate();
         }
 
-        public class Recounter implements Runnable {
-            @Override
-            public void run() {
-                for (int i = 0; i < WIDTH; ++i)
-                    for (int j = 0; j < HEIGHT; ++j) {
-                        new_step_field[i][j] = field[i][j];
-                    }
-                for (int i = 0; i < WIDTH; i++) {
-                    for (int j = 0; j < HEIGHT; j++) {
-                        if (i < WIDTH - 1 && (field[i + 1][j] + 1) % 16 == field[i][j]) {
-                            new_step_field[i][j] = field[i + 1][j];
-                        } else if (j < HEIGHT - 1 && (field[i][j + 1] + 1) % 16 == field[i][j]) {
-                            new_step_field[i][j] = field[i][j + 1];
-                        } else if (i < WIDTH - 1 && j < HEIGHT - 1 && (field[i + 1][j + 1] + 1) % 16 == field[i][j]) {
-                            new_step_field[i][j] = field[i + 1][j + 1];
-                        } else if (i > 0 && (field[i - 1][j] + 1) % 16 == field[i][j]) {
-                            new_step_field[i][j] = field[i - 1][j];
-                        } else if (j > 0 && (field[i][j - 1] + 1) % 16 == field[i][j]) {
-                            new_step_field[i][j] = field[i][j - 1];
-                        } else if (i > 0 && j > 0 && (field[i - 1][j - 1] + 1) % 16 == field[i][j]) {
-                            new_step_field[i][j] = field[i - 1][j - 1];
-                        } else if (i > 0 && j < HEIGHT - 1 && (field[i - 1][j + 1] + 1) % 16 == field[i][j]) {
-                            new_step_field[i][j] = field[i - 1][j + 1];
-                        } else if (i < WIDTH - 1 && j > 0 && (field[i + 1][j - 1] + 1) % 16 == field[i][j]) {
-                            new_step_field[i][j] = field[i + 1][j - 1];
-                        }
-                    }
-                }
-                try {
-                    this.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                invalidate();
-            }
 
-
-        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(new WhirlView(this));
 
     }
