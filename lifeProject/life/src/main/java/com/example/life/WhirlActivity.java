8ae0855aa13dee557fb30024e642f375f922cfa7
Ruslan <akhundov2@gmail.com>
2013-09-11 10:48:01 +0400

    /

--- lifeProject/life/src/main/java/com/example/life/WhirlActivity.java
+++ lifeProject/life/src/main/java/com/example/life/WhirlActivity.java
@@ -9,7 +9,6 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.view.SurfaceView;
 import android.graphics.Color;
-import android.graphics.Bitmap;
 import android.view.WindowManager;
 
 import java.util.Random;
